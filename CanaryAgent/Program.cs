c3de060d2aee924a3ef90d639b35cb8e044d0c06
rqyh75 <alhashmiruqaiyah@gmail.com>
2026-05-08 16:26:01 +0400

    Add files via upload

---
+++ CanaryAgent/Program.cs
@@ -0,0 +1,15 @@
+﻿//This is the main program
+using System.Threading.Tasks;
+using CanaryAgent.Core;
+
+namespace CanaryAgent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var agent = new Agent();
+            agent.RunContinuously();
+        }
+    }
+}

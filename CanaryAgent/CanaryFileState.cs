cd0a681afd978d9a590ea5827feeb2f7522c9b40
rqyh75 <alhashmiruqaiyah@gmail.com>
2026-05-08 16:23:17 +0400

    Add files via upload

---
+++ CanaryAgent/CanaryFileState.cs
@@ -0,0 +1,18 @@
+﻿namespace CanaryAgent.Storage
+{
+    public class CanaryFileState
+    {
+        public string PersonaId { get; set; } = "";
+        public string LogicalName { get; set; } = "";
+        public string FullPath { get; set; } = "";
+        public string FileType { get; set; } = "";
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public long LastObservedSize { get; set; }
+        public DateTime LastObservedWriteTime { get; set; }
+        public string? LastObservedHash { get; set; }
+        public DateTime NextModificationTime { get; set; }
+        public DateTime NextRenameTime { get; set; }
+
+    }
+}

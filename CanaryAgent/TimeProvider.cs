55bc53df9408a53aeb9fee0c24607db0952d6d56
rqyh75 <alhashmiruqaiyah@gmail.com>
2026-05-08 16:25:30 +0400

    Add files via upload

---
+++ CanaryAgent/TimeProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace CanaryAgent.Utils
+{
+    internal class TimeProvider
+    {
+        public virtual DateTime Now()
+        {
+            return DateTime.Now;
+        }
+        public bool IsNewDay(DateTime lastRun)
+        {
+            return Now().Date > lastRun.Date;
+        }
+    }
+}
